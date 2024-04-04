494810855e0d02310c14c8a6bb5f99f7edb75964
Yarelis Martin <yarelis0martin@gmail.com>
2024-04-03 19:12:50 -0500

    finished setting up joke.js

--- components/Joke.js
+++ components/Joke.js
@@ -1,10 +1,19 @@
-// import React from 'react';
+import React from 'react';
+import { PropTypes } from 'react-bootstrap/esm/Image';
 
-// export default function Joke({ joke, btnText }) {
-//   return (
-//     <>
-//       <h1>{joke.setup}</h1>
-//       { btnText === 'Get Punchline!' ? <h2>{joke.delivery}</h2> : ''};
-//     </>
-//   );
-// }
+export default function Joke({ joke, btnText }) {
+  return (
+    <>
+      <h1>{joke.setup}</h1>
+      { btnText === 'Get Punchline!' ? <h2>{joke.delivery}</h2> : ''};
+    </>
+  );
+}
+
+Joke.propTypes = {
+  joke: PropTypes.shape({
+    setup: PropTypes.string.isRequired,
+    delivery: PropTypes.string.isRequired,
+  }).isRequired,
+  btnText: PropTypes.string.isRequired,
+};
